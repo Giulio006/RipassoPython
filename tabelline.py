db45736df557b2a52f15c2ebc86bd659ba991931
Giulio006 <145097581+Giulio006@users.noreply.github.com>
2024-09-13 13:25:25 +0200

    Update tabelline.py

--- tabelline.py
+++ tabelline.py
@@ -1,4 +1,4 @@
-#la m dopo il nome del file sta per modificato
+#tabellina del 4
 x = 0
-for i in range(4, 41 ,4): #il range non arriva mai al numero finale quindi faccio fino a 41
-    print(i)              #range(inizio, fine, incremento)
+for i in range(4, 41 ,4):
+    print(i)
