76ff535201960cced74d5d8296f8f0936a5ccc74
Giulio006 <145097581+Giulio006@users.noreply.github.com>
2024-09-13 13:09:10 +0200

    Create multiplication of 4

---
+++ tabelline.py
@@ -0,0 +1,4 @@
+#la m dopo il nome del file sta per modificato
+x = 0
+for i in range(4, 41 ,4): #il range non arriva mai al numero finale quindi faccio fino a 41
+    print(i)              #range(inizio, fine, incremento)
